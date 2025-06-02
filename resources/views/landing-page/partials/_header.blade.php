ead3307054fd48d91d69c87add38273bb9f60daa
Rajesh Kumar <rajeshkumar05071999@gmail.com>
2025-06-02 16:27:47 +0530

    changes

--- resources/views/landing-page/partials/_header.blade.php
+++ resources/views/landing-page/partials/_header.blade.php
@@ -263,7 +263,7 @@
                       <!-- Wishlist -->
                       @if(empty(auth()->user()) || auth()->user()->user_type !== 'user')
                         <li class="ms-sm-3 ms-2">
-                           <a href="{{url('login')}}" class="btn btn login-btn" role="button">
+                           <a href="{{url('login')}}" class="btn btn login-btn " role="button">
                               <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16"
                                  fill="none">
                                  <path fill-rule="evenodd" clip-rule="evenodd"
