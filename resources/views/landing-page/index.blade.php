c12ff064c8b7d83dd7460851f46db0338ed04177
Rajesh Kumar <rajeshkumar05071999@gmail.com>
2025-07-02 19:33:04 +0530

    changes

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -142,14 +142,19 @@
 		</div> -->
 
 	<div class="col-xl-6 px-xl-0 mt-xl-0 mt-5">
-		<div class="sliderr" id="sliderr">
+		<!-- <div class="sliderr" id="sliderr">
 		<div class="slidess" id="slidess">
 			<div class="slidee" >Plumbers</div>
 			<div class="slidee" >Stores</div>
 			<div class="slidee" >Categories</div>
 			<div class="slidee" >Electronics</div>
 		</div>
-		</div>
+		</div> -->
+
+
+		<div class="typing-animation"><a href="" class="typewrite" data-period="2000" data-type="[ &quot;Directory&quot;, &quot;Deal&quot;, &quot;People&quot;, &quot;Local&quot; ,&quot;Shopping&quot; ]">
+          <span class="wrap">&nbsp;</span>
+        </a></div>
 	</div>
 
 
@@ -1775,6 +1780,69 @@ sliderr.addEventListener('mouseleave', startSlider);
 // Initialize slider
 slidess.style.transition = 'transform 0.5s ease-in-out';
 startSlider();
+
+</script>
+
+
+<script>
+	var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #d29c49}";
+  document.body.appendChild(css);
+};
 
 </script>
 @endsection
