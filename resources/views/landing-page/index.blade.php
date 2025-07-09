e491c714d47bd23e65719b1d4ea6d693cd63dc51
Rajesh Kumar <rajeshkumar05071999@gmail.com>
2025-07-09 19:27:13 +0530

    changes

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -880,9 +880,18 @@
 				
 				           <ul class="nav nav-tabs custom-tabs justify-content-center" id="myTab" role="tablist">
 							<li class="nav-item nav-item-1" role="presentation">
-							<div  class="nav-link1 active" id="news-tab" data-bs-toggle="tab" data-bs-target="#news">
+							<div  class="nav-link1 " id="allwebs-tab" data-bs-toggle="tab" data-bs-target="#allwebs">
 								<div class="service_box service-box-circle ">
-									<div class="img-box"><img src="{{asset('images/home/news.png')}}" class="img-fluid" /></div>
+									<div class="img-box active"><img src="{{asset('images/home/all-list.png')}}" class="img-fluid" /></div>
+									<h5><a href="#">All</a></h5>
+								</div>
+							</div>
+						    </li>
+
+							<li class="nav-item nav-item-1" role="presentation">
+							<div  class="nav-link1 " id="news-tab" data-bs-toggle="tab" data-bs-target="#news">
+								<div class="service_box service-box-circle ">
+									<div class="img-box active"><img src="{{asset('images/home/news.png')}}" class="img-fluid" /></div>
 									<h5><a href="#">News</a></h5>
 								</div>
 							</div>
@@ -923,6 +932,70 @@
 					<div class="tab-content mt-5" id="myTabContent">
 
                   <!-- =============Webs tab content=============== -->
+
+				   <!-- allWebs Tab -->
+						<div class="tab-pane fade" id="allwebs" role="tabpanel">
+						<div class="row">
+							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
+								<div class="service_box">
+									<div class="img-box"><img src="https://dailycleaners.co.nz/dollarshopdev/public/images/home/windows.png" class="img-fluid" /></div>
+									<h5><a href="#">Window Tinting</a></h5>
+								</div>
+
+							</div>
+							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
+								<div class="service_box">
+									<div class="img-box"><img src="https://dailycleaners.co.nz/dollarshopdev/public/images/home/water.png" class="img-fluid" /></div>
+									<h5><a href="#">Water Blasting</a></h5>
+								</div>
+
+							</div>
+
+							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
+								<div class="service_box">
+									<div class="img-box"><img src="https://dailycleaners.co.nz/dollarshopdev/public/images/home/windows.png" class="img-fluid" /></div>
+									<h5><a href="#">Window Tinting</a></h5>
+								</div>
+
+							</div>
+							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
+								<div class="service_box">
+									<div class="img-box"><img src="https://dailycleaners.co.nz/dollarshopdev/public/images/home/water.png" class="img-fluid" /></div>
+									<h5><a href="#">Water Blasting</a></h5>
+								</div>
+
+							</div>
+
+							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
+								<div class="service_box">
+									<div class="img-box"><img src="https://dailycleaners.co.nz/dollarshopdev/public/images/home/windows.png" class="img-fluid" /></div>
+									<h5><a href="#">Window Tinting</a></h5>
+								</div>
+
+							</div>
+							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
+								<div class="service_box">
+									<div class="img-box"><img src="https://dailycleaners.co.nz/dollarshopdev/public/images/home/water.png" class="img-fluid" /></div>
+									<h5><a href="#">Water Blasting</a></h5>
+								</div>
+
+							</div>
+
+							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
+								<div class="service_box" data-bs-toggle="modal" data-bs-target="#exampleModal">
+									<div class="plus-icon">
+										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+											<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+										</svg>
+									</div>
+									<h5><a href="#">Add</a></h5>
+								</div>
+
+							</div>
+
+                         </div>
+                        </div>
+
 					<!-- news Tab -->
 					<div class="tab-pane fade show active" id="news" role="tabpanel">
 						<div class="row">
@@ -942,7 +1015,7 @@
 							</div>
 
 							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
-								<div class="service_box">
+								<div class="service_box" data-bs-toggle="modal" data-bs-target="#exampleModal">
 									<div class="plus-icon">
 										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
 											<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
@@ -975,7 +1048,7 @@
 							</div>
 
 							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
-								<div class="service_box">
+								<div class="service_box" data-bs-toggle="modal" data-bs-target="#exampleModal">
 									<div class="plus-icon">
 										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
 											<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
@@ -1008,7 +1081,7 @@
 							</div>
 
 							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
-								<div class="service_box">
+								<div class="service_box" data-bs-toggle="modal" data-bs-target="#exampleModal">
 									<div class="plus-icon">
 										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
 											<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
@@ -1042,7 +1115,7 @@
 							</div>
 
 							<div class="col-lg-2 col-md-3 col-sm-4 col-4">
-								<div class="service_box">
+								<div class="service_box" data-bs-toggle="modal" data-bs-target="#exampleModal">
 									<div class="plus-icon">
 										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
 											<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
@@ -1083,6 +1156,38 @@
 		</div>
 	</div>
 </div>
+
+
+<!-- ==================webs modal =================== -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Web</h5>
+        <span class="text-primary custom-btn-close" data-dismiss="modal" aria-label="Close" onclick="jQuery('#exampleModal').modal('hide');">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="41" viewBox="0 0 40 41" fill="none">
+                        <rect x="12" y="11.8381" width="17" height="17" fill="white"></rect>
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M12.783 4.17017H27.233C32.883 4.17017 36.6663 8.13683 36.6663 14.0368V27.6552C36.6663 33.5385 32.883 37.5035 27.233 37.5035H12.783C7.13301 37.5035 3.33301 33.5385 3.33301 27.6552V14.0368C3.33301 8.13683 7.13301 4.17017 12.783 4.17017ZM25.0163 25.8368C25.583 25.2718 25.583 24.3552 25.0163 23.7885L22.0497 20.8218L25.0163 17.8535C25.583 17.2885 25.583 16.3552 25.0163 15.7885C24.4497 15.2202 23.533 15.2202 22.9497 15.7885L19.9997 18.7535L17.033 15.7885C16.4497 15.2202 15.533 15.2202 14.9663 15.7885C14.3997 16.3552 14.3997 17.2885 14.9663 17.8535L17.933 20.8218L14.9663 23.7718C14.3997 24.3552 14.3997 25.2718 14.9663 25.8368C15.2497 26.1202 15.633 26.2718 15.9997 26.2718C16.383 26.2718 16.7497 26.1202 17.033 25.8368L19.9997 22.8885L22.9663 25.8368C23.2497 26.1385 23.6163 26.2718 23.983 26.2718C24.3663 26.2718 24.733 26.1202 25.0163 25.8368Z"
+                            fill="currentColor">
+                        </path>
+                  </svg>
+        </span>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal" aria-label="Close" onclick="jQuery('#exampleModal').modal('hide');">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- ==================webs modal =================== -->
 
 
 <div class="section-padding-custom bg-light">
